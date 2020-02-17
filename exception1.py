002698e32057fa61ab0a3ec34ede1abb246e100b
AlexanderLyaksa <sanchos2004@gmail.com>
2020-02-17 23:00:27 +0300

    For checking

--- exception1.py
+++ exception1.py
@@ -9,12 +9,28 @@
   и завершала работу при помощи оператора break
     
 """
+question = {"Как дела": "Хорошо!", "Что делаешь?": "Программирую", 'Как погода?': 'Солнечно!'}
+
+
+def ask_user_dict():
+    # не учитываем регистр вопроса
+    raw_user_say = input('Введите вопрос: ')
+    user_say = raw_user_say.capitalize()
+    if user_say in question:
+        print(question[user_say])
+
 
 def ask_user():
     """
     Замените pass на ваш код
     """
-    pass
-    
+    while True:
+        try:
+            ask_user_dict()
+        except KeyboardInterrupt:
+            print('Пока!')
+            break
+
+
 if __name__ == "__main__":
     ask_user()
