b2592a1b1535002dc83477a72de55cc1c5832ecf
AlexanderLyaksa <sanchos2004@gmail.com>
2020-02-18 15:58:30 +0300

    На проверку

--- ephem_bot.py
+++ ephem_bot.py
@@ -12,23 +12,38 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
-import logging
-
+import logging, ephem
+from my_data import API_KEY, PROXY
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
-)
+                    )
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn', 
-        'password': 'python'
-    }
+PLANETS_MAP = {
+    'mars': ephem.Mars(),
+    'venus': ephem.Venus(),
+    'mercury': ephem.Mercury(),
+    'jupiter': ephem.Jupiter(),
+    'saturn': ephem.Saturn(),
+    'uranus': ephem.Uranus(),
+    'neptune': ephem.Neptune(),
 }
+
+
+def find_cons(bot, update):
+    if len(update.message.text.split()) == 2:
+        name_planet = update.message.text.split()[1]
+        planet = name_planet.lower()
+        if planet in PLANETS_MAP:
+            temp = PLANETS_MAP[planet]
+            temp.compute()
+            constellation = ephem.constellation(temp)
+            update.message.reply_text(constellation[1])
+        else:
+            update.message.reply_text('Этой планеты нет в Солнечной системе :)')
 
 
 def greet_user(bot, update):
@@ -38,21 +53,22 @@ def greet_user(bot, update):
 
 
 def talk_to_me(bot, update):
-    user_text = update.message.text 
+    user_text = update.message.text
     print(user_text)
     update.message.reply_text(user_text)
- 
+
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
-    
+    mybot = Updater(API_KEY, request_kwargs=PROXY)
+
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", find_cons))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    
+
     mybot.start_polling()
     mybot.idle()
-       
+
 
 if __name__ == "__main__":
     main()
