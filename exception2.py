fb6fa74cf1ca9918d0af9c8cad3aaa722aab0d2b
AlexanderLyaksa <sanchos2004@gmail.com>
2020-02-20 16:05:51 +0300

    ДЗ после code review 1

--- exception2.py
+++ exception2.py
@@ -24,9 +24,9 @@ def get_summ(num_one, num_two):
 
 
 if __name__ == "__main__":
-    print(get_summ(2, 2))
-    print(get_summ(3, "3"))
-    print(get_summ("4", "4"))
-    print(get_summ("five", 5))
-    print(get_summ("six", "шесть"))
+    assert get_summ(2, 2) == 4
+    assert get_summ(3, "3") == 6
+    assert get_summ("4", "4") == 8
+    assert get_summ("five", 5) is None
+    assert get_summ("six", "шесть") is None
 
