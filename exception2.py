ec53b1a300b7d8d1b81f14dbeff304e346b7f288
gitrus <gitrus@yandex.com>
2020-03-01 16:33:41 +0300

    Done exception_2

--- exception2.py
+++ exception2.py
@@ -16,8 +16,13 @@ def get_summ(num_one, num_two):
     """
     Замените pass на ваш код
     """
-    pass
-    
+    try:
+        sum = int(num_one) + int(num_two)
+    except ValueError:
+        return None
+    else:
+        return sum
+
 if __name__ == "__main__":
     print(get_summ(2, 2))
     print(get_summ(3, "3"))
