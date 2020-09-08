561b7ffcbffbf6678cb44c8b77df93cdbf6c2d42
Mikhail Spiridonov <Muxa2793@mail.ru>
2020-09-08 22:23:51 +0300

    Задание 1 вер.1

--- 1_if1.py
+++ 1_if1.py
@@ -15,11 +15,41 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    age = input('Введите ваш возраст:') 
+
+    def your_destiny(age):
+
+      
+      #функция для добавления правильного окончания к возрасту:
+      def age_ending(age):
+
+        if len(str(age)) == 1 and int(age) == 0:
+          return 'лет'
+        if len(str(age)) == 1 and int(age) == 1:
+          return 'год'
+        elif len(str(age)) == 1 and 2 <= int(age) <= 4:
+          return 'года'
+        elif len(str(age)) == 1 and 5 <= int(age) <= 9:
+          return 'лет'
+        elif 2 <= len(str(age)) and 10 <= int(age) <= 20:
+          return 'лет'
+        elif 2 <= len(str(age)):
+          return age_ending(int(age)%10)
+
+      if age.isdigit() or type(age) == int:
+        if 1 <= int(age) <= 5:
+          return print(f'Ваш полный возраст: {age} {age_ending(age)}.\nВам суждено учиться в детском саду')
+        elif 5 < int(age) <= 17:
+          return print(f'Вашage  полный возраст: {age} {age_ending(age)}.\nВам суждено учиться в школе')
+        elif 17 < int(age) <= 150:
+          return print(f'Ваш полный возраст: {age} {age_ending(age)}.\nВам суждено учиться в ВУЗе или работать')
+        elif int(age) > 150:
+          return print(f'Ваш полный возраст: {age} {age_ending(age)}.\nУкажите ваш настоящий возраст!')
+        elif int(age) == 0:
+          return print(f'Ваш полный возраст: {age} {age_ending(age)}.\nВаша судьба неопределена')
+      else:
+        return print('Возраст указан неверно. Должно быть целое и положительное число')
+    your_destiny(age)
 
 if __name__ == "__main__":
     main()
