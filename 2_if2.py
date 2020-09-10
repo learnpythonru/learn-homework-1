1516d6e7c9785b268b73d86a8176e7a146a95096
skhomm <i.kwibus@gmail.com>
2020-09-10 19:21:53 +0300

    Complete second task

--- 2_if2.py
+++ 2_if2.py
@@ -20,7 +20,20 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    def check_strings(string_1, string_2):
+        if type(string_1) != str or type(string_2) != str:
+            return 0
+        elif string_1 == string_2:
+            return 1
+        elif (len(string_1) > len(string_2)) and string_2 != "learn":
+            return 2
+        elif string_2 == "learn":
+            return 3
+
+    print(check_strings(42, "bebebe"))
+    print(check_strings("bebebe", "bebebe"))
+    print(check_strings("baububupi", "bebebe"))
+    print(check_strings("bububu", "learn"))
+
 if __name__ == "__main__":
     main()
