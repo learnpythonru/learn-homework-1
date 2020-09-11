ef0124c7fbbcd5f165e77769f062ae13a2ae34e1
Оревков Николай Степанович <NOrevkov@ibs.root.ibscorp.ru>
2020-09-11 21:31:02 +0300

    homework 8

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -15,44 +15,55 @@
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+import ephem
+from datetime import date
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
 )
 
+PROXY = {'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn', 
-        'password': 'python'
-    }
-}
+def main():
+    mybot = Updater(settings.API_KEY, request_kwargs=PROXY)
+    
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
+    mybot.start_polling()
+    mybot.idle()
 
 def greet_user(bot, update):
     text = 'Вызван /start'
     print(text)
     update.message.reply_text(text)
 
-
 def talk_to_me(bot, update):
     user_text = update.message.text 
     print(user_text)
     update.message.reply_text(user_text)
- 
 
-def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
-    
-    dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    
-    mybot.start_polling()
-    mybot.idle()
-       
+def planet(bot, update):
+    dict_planets = {'Mercury' : ephem.Mercury, 
+                    'Venus'   : ephem.Venus, 
+                    'Mars'    : ephem.Mars, 
+                    'Jupiter' : ephem.Jupiter, 
+                    'Saturn'  : ephem.Saturn, 
+                    'Uranus'  : ephem.Uranus, 
+                    'Neptune' : ephem.Neptune,
+                    'Pluto'   : ephem.Pluto}
+    user_planet = update.message.text.split()[1].lower().capitalize()
+    if user_planet in dict_planets:
+      constellation = ephem.constellation(dict_planets[user_planet](date.today()))
+      reply_text = f'{user_planet} is currently in the constellation of {constellation[1]}'
+    else:
+      reply_text = 'Unknown planet...'
+    update.message.reply_text(reply_text)
 
 if __name__ == "__main__":
     main()
