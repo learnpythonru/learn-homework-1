620fa8c720310e203a2f2e7d0e2fe5baf7e26b84
Nikolay <nikolay.orevkov@gmail.com>
2020-09-11 22:49:02 +0300

    Revert "Revert "Revert "Homewor 1-5 added"""
    This reverts commit b27d4dc9231deaf2024f6fac02b0d6fb6c0e0c8c.

--- 3_for.py
+++ 3_for.py
@@ -11,17 +11,11 @@
 """
 
 def main():
-  s = 0
-  l = 0
-  d = [{'school_class': '4a', 'scores': [3,4,4,5,2]},\
-      {'school_class': '5a', 'scores': [2,5,4,5]},\
-      {'school_class': '5a', 'scores': [5,1,2,1,1]}]
-  for i in d:
-      s = s + sum(i['scores'])
-      l = l + len(i['scores'])
-      print('Cредний балл по классу', i['school_class'], ':', sum(i['scores'])/len(i['scores']))
-
-  print('Cредний балл по всей школе: ', s/l)
+    """
+    Эта функция вызывается автоматически при запуске скрипта в консоли
+    В ней надо заменить pass на ваш код
+    """
+    pass
     
 if __name__ == "__main__":
     main()
