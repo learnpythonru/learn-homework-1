6ca9e23b3e2ae8dfe828475b6e3c955b08d3a2ec
Andrew Starkov <andrewstarkov@MacBook-Pro-Dara.local>
2020-09-11 21:18:13 +0300

    Посчитал строки

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,17 +13,13 @@
 """
 
 def get_summ(num_one, num_two):
-    """
-    Замените pass на ваш код
-    """
-    try:
-      isinstance(num_one, int) and isinstance(num_two, int):
-      num_three = num_one + num_three
-      print (num_three)
-    except ValueError:
-      print("lskdc")
-    
+  try:
+    return int(num_one) + int(num_two)
+  except (ValueError, TypeError):
+    print("ValueError")
 
+  
+    
 if __name__ == "__main__":
     print(get_summ(2, 2))
     print(get_summ(3, "3"))
