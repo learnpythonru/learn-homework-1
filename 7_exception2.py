4eaa40be382cf15a1d7950157d36dd8e0cf2ff6b
Andrew Starkov <andrewstarkov@MacBook-Pro-Dara.local>
2020-09-11 15:45:37 +0300

    Шото получилось

--- 7_exception2.py
+++ 7_exception2.py
@@ -16,8 +16,14 @@ def get_summ(num_one, num_two):
     """
     Замените pass на ваш код
     """
-    pass
+    try:
+      isinstance(num_one, int) and isinstance(num_two, int):
+      num_three = num_one + num_three
+      print (num_three)
+    except ValueError:
+      print("lskdc")
     
+
 if __name__ == "__main__":
     print(get_summ(2, 2))
     print(get_summ(3, "3"))
