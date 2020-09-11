1d30d92c401d5c30fd66bc394ef6eac98425762d
skhomm <i.kwibus@gmail.com>
2020-09-11 19:49:45 +0300

    Complete sixth task

--- 6_exception1.py
+++ 6_exception1.py
@@ -14,7 +14,20 @@ def ask_user():
     """
     Замените pass на ваш код
     """
-    pass
-    
+    answers = {
+        'Как дела?': 'Восхитительно!',
+        'Что делаешь?': 'Прокрастинирую',
+        'Как тебя зовут?': 'Антошка'
+    }
+
+    try:
+        while True:
+            question = input("Спроси меня\n")
+            for answer in answers:
+                if answer == question:
+                    print(answers[question])
+    except KeyboardInterrupt:
+        print("\nПока!")
+
 if __name__ == "__main__":
     ask_user()
