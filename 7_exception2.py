8bedc4b7ed45b69247d0b8f56b846dcdeab06acb
skhomm <i.kwibus@gmail.com>
2020-09-11 21:05:31 +0300

    Complete seventh task

--- 7_exception2.py
+++ 7_exception2.py
@@ -16,11 +16,17 @@ def get_summ(num_one, num_two):
     """
     Замените pass на ваш код
     """
-    pass
-    
+    try:
+        summ = int(num_one) + int(num_two)
+        return summ
+    except ValueError:
+        print("Принимаю только целые")
+
 if __name__ == "__main__":
     print(get_summ(2, 2))
     print(get_summ(3, "3"))
     print(get_summ("4", "4"))
     print(get_summ("five", 5))
     print(get_summ("six", "шесть"))
+
+# Как сделать, чтобы не печаталось None?
