f57e673af923465f510ceebc1191af76e0826b4a
aslepaugo <aslepaugo@gmail.com>
2020-09-13 09:33:04 +0300

    Transition to Version 12.0

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -4,11 +4,11 @@
 Использование библиотек: ephem
 
 * Установите модуль ephem
-* Добавьте в бота команду /planet, которая будет принимать на вход 
+* Добавьте в бота команду /planet, которая будет принимать на вход
   название планеты на английском, например /planet Mars
-* В функции-обработчике команды из update.message.text получите 
+* В функции-обработчике команды из update.message.text получите
   название планеты (подсказка: используйте .split())
-* При помощи условного оператора if и ephem.constellation научите 
+* При помощи условного оператора if и ephem.constellation научите
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
@@ -18,41 +18,40 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
-                    filename='bot.log'
-)
+                    filename='bot.log')
 
 
 PROXY = {
     'proxy_url': 'socks5://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {
-        'username': 'learn', 
+        'username': 'learn',
         'password': 'python'
     }
 }
 
 
-def greet_user(bot, update):
+def greet_user(update, context):
     text = 'Вызван /start'
     print(text)
-    update.message.reply_text(text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 
-def talk_to_me(bot, update):
-    user_text = update.message.text 
+def talk_to_me(update, context):
+    user_text = update.message.text
     print(user_text)
-    update.message.reply_text(user_text)
- 
+    context.bot.send_message(chat_id=update.effective_chat.id, text=user_text)
+
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
-    
+    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    
+
     mybot.start_polling()
     mybot.idle()
-       
+
 
 if __name__ == "__main__":
     main()
