c92e44f029a511909b42429e43a168f16dd206d1
n.brusnika <n.brusnika@visionlabs.ru>
2020-09-16 17:51:37 +0400

    complete

--- 6_exception1.py
+++ 6_exception1.py
@@ -1,20 +1,20 @@
-"""
-
-Домашнее задание №1
-
-Исключения: KeyboardInterrupt
-
-* Перепишите функцию ask_user() из задания while2, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
-  и завершала работу при помощи оператора break
-    
-"""
-
 def ask_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+    questions = {'qst': 'how are you?', 'ans': 'Good!', 'qst1': 'what are you doing?', 'ans1': 'I\'m slepping!'}
+
+    while True:
+        try:
+            say = input('input your questions:')
+            if say.lower() == questions['qst']:
+                print(questions['ans'])
+            elif say.lower() == questions['qst1']:
+                print(questions['ans1'])
+            else:
+                print('I don\'t understand')
+
+        except KeyboardInterrupt:
+            print('\nBye!Bye!')
+            break
+
+
 if __name__ == "__main__":
     ask_user()
