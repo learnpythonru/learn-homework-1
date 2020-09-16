03f80904202c0d095c47e08ecafba952a8805d09
n.brusnika <n.brusnika@visionlabs.ru>
2020-09-16 18:25:51 +0400

    complete

--- 7_exception2.py
+++ 7_exception2.py
@@ -12,12 +12,15 @@
     
 """
 
+
 def get_summ(num_one, num_two):
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+    try:
+        summ = int(num_one) + int(num_two)
+        print(summ)
+
+    except ValueError:
+        print('error')
+
 if __name__ == "__main__":
     print(get_summ(2, 2))
     print(get_summ(3, "3"))
