870b49d46a559e7f2e3867a213ad063985795352
sh1n0b1n0m0n0 <nindzja.t001@gmail.com>
2020-11-22 19:06:03 +0300

    first commit

--- 2_if2.py
+++ 2_if2.py
@@ -20,7 +20,29 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+
+    def compare_2_strings(str1, str2):
+        if (type(str1) != str) or (type(str2) != str):
+            return 0
+        elif str1 == str2:
+            return 1
+        elif str1 != str2 and len(str1) > len(str2):
+            if str2 == 'learn':
+                return 3
+            return 2
+        elif str1 != str2 and str2 == 'learn':
+            return 3
+
+    print(compare_2_strings(1, 'pdsfsfsfs'))
+    print(compare_2_strings('pdsfsfsfs', 312))
+    print(compare_2_strings('sreee', 'sreee'))
+    print(compare_2_strings('sreee32424', 'tr3eee'))
+    print(compare_2_strings('Skr', 'skreee'))  # По условию не проходит)
+    print(compare_2_strings('Skr', 'learn'))
+    print(compare_2_strings('qeqedsd', 'learn'))
+    print(compare_2_strings(123, 'learn'))
+    print(compare_2_strings('learn', 'sdsdar'))  # По условию не проходит)
+
+
 if __name__ == "__main__":
     main()
