1671f98681fb2e5e41b35abf3b7a5511e4853b98
nickr <nickro@ya.ru>
2021-02-15 23:56:19 +0300

    seventh task is done

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,12 +13,27 @@
     
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+
+def discounted(price, discount, max_discount=20):
+    try:
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = int(max_discount)
+
+    except ValueError as exp:
+        return exp
+
+    except TypeError as exp:
+        return exp
+
+    if discount >= max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price - (price * discount / 100)
+
+    return price_with_discount
+
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
