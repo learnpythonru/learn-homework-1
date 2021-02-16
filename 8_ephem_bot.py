b07b81c0963a0734b4a2d9b908de583ef2cba99f
nickr <nickro@ya.ru>
2021-02-16 21:47:03 +0300

    eighth is done

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,8 +12,11 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
-import logging
+import datetime
 
+import ephem
+import logging
+from settings import TOKEN
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -21,13 +24,13 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     filename='bot.log')
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
+# PROXY = {
+#     'proxy_url': 'socks5://t1.learn.python.ru:1080',
+#     'urllib3_proxy_kwargs': {
+#         'username': 'learn',
+#         'password': 'python'
+#     }
+# }
 
 
 def greet_user(update, context):
@@ -39,14 +42,42 @@ def greet_user(update, context):
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(user_text)
+
+
+def planet(update, context):
+    print('Вызван /planet')
+    user_text = update.message.text.split()
+    # # print(user_text)
+    user_planet = user_text[1].capitalize()
+    tomorrow = (datetime.datetime.today() + datetime.timedelta(days=1)).strftime('%Y/%m/%d')
+    if user_planet == 'Mercury':
+        constellation = ephem.constellation(ephem.Mercury(tomorrow))
+    elif user_planet == 'Venus':
+        constellation = ephem.constellation(ephem.Venus(tomorrow))
+    elif user_planet == 'Earth':
+        constellation = ephem.constellation(ephem.Earth(tomorrow))
+    elif user_planet == 'Mars':
+        constellation = ephem.constellation(ephem.Mars(tomorrow))
+    elif user_planet == 'Jupiter':
+        constellation = ephem.constellation(ephem.Jupiter(tomorrow))
+    elif user_planet == 'Saturn':
+        constellation = ephem.constellation(ephem.Saturn(tomorrow))
+    elif user_planet == 'Uranus':
+        constellation = ephem.constellation(ephem.Uranus(tomorrow))
+    elif user_planet == 'Neptune':
+        constellation = ephem.constellation(ephem.Neptune(tomorrow))
+    else:
+        constellation = f'I dont know planet {user_planet}!'
+
+    update.message.reply_text(constellation)
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
-
+    mybot = Updater(TOKEN, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
