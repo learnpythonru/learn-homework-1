364f464a333624b656fb0c5ad3fe8bfafff1ba63
Vladimir Gulevsky <Vladimir.Gulevsky@gmail.com>
2021-02-21 02:44:10 +0300

    Первые два задания

--- 2_if2.py
+++ 2_if2.py
@@ -10,17 +10,25 @@
 * Если строки одинаковые, вернуть 1
 * Если строки разные и первая длиннее, вернуть 2
 * Если строки разные и вторая строка 'learn', возвращает 3
-* Вызвать функцию несколько раз, передавая ей разные праметры 
+* Вызвать функцию несколько раз, передавая ей разные параметры 
   и выводя на экран результаты
 
 """
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+def main(string1, string2):
+
+    if (isinstance(string1, str) is True) and (isinstance(string2, str) is True):
+        if string1 == string2: 
+          return 1
+        elif len(string1) > len(string2):
+          return 2
+        elif string2 == "learn":
+          return 3
+    else:
+        return "0"
     
+string1 = input("Введите строку " )
+string2 = input("Введите строку " )
+
 if __name__ == "__main__":
-    main()
+    print(main(string1, string2))
