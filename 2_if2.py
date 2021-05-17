3300eef38a3db57a70cd387058384c8e412b6492
Buzhak <Byzhak@ya.ru>
2021-05-17 23:41:42 +0300

    task_2

--- 2_if2.py
+++ 2_if2.py
@@ -14,13 +14,26 @@
   и выводя на экран результаты
 
 """
-
+def compare_str(str0,str1) -> int:
+    if type(str0) != str or type(str1) != str:
+        return 0
+    elif str0 != str1 and str1 == 'learn':
+        return 3
+    elif str0 != str1 and len(str0)>len(str1):
+        return 2
+    else: return 1
+    
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    
+    print(compare_str(1,2))
+    print(compare_str(1,'hello'))
+    print(compare_str('hello','hello'))
+    print(compare_str('hello_world','hello'))
+    print(compare_str('hello_world','learn'))
     
 if __name__ == "__main__":
     main()
