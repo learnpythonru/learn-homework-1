067cc51ae3e3f8da9becab0be035a036332dd230
Konstantin L <kosto55@mail.ru>
2021-05-17 19:02:12 +0300

    Done

--- 3_for.py
+++ 3_for.py
@@ -18,14 +18,31 @@ school = [
 {'school_class': '4c', 'scores': [2,3,3,4,1]},
 ]
 
-def main():
-    score_avg = 0
-  
-    for i in school:
-        score_avg += sum(school[i]['scores']) / len(school[i]['scores'])
-    print(score_avg)
-
-
+def main(school_avg, class_avg):
+    school_avg_score = []
+    for i in school_avg:
+        for j in i.values():
+            if type(j) == list:
+                school_avg_score.append(sum(j) / len(j))
+            else:
+                continue 
+    school_avg_score = sum(school_avg_score) / len(school_avg_score)   
+    print(f'Средний бал по всей школе: {school_avg_score}')
     
+    score_avg = 0
+    class_name =''
+    for i in school_avg:
+        for j in i.values():
+            if type(j) == list:
+                score_avg += sum(j) / len(j)
+                print(f'Средний бал класса {class_name} равен {score_avg}')
+                score_avg = 0
+
+            elif type(j) == str:
+                class_name = j
+
+            else:
+                continue 
+
 if __name__ == "__main__":
-    main()
+    main(school, school)
