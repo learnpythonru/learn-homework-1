b001ae7be5f5e9b617e91e82b80923cee5590985
Konstantin L <kosto55@mail.ru>
2021-05-20 23:07:41 +0300

    Done v0.03

--- 7_exception2.py
+++ 7_exception2.py
@@ -24,36 +24,21 @@ def discounted(price, discount, max_discount=50, name=''):
 
     try:
         price = abs(float(price))
-    except(TypeError, ValueError):
-        print('Введены неверные параметры')
-        price = 0
-    
-    try:
         discount = abs(float(discount))
-    except(TypeError, ValueError):
-        print('Введены неверные параметры')
-        discount = 0        
-    
-    try:    
         max_discount = abs(int(max_discount))
+
+        if max_discount > 99:
+            raise ValueError('Максимальная скидка не может быть больше 99%')
+
+        if discount >= max_discount or 'iphone' in name.lower() or not name:
+            price_with_discount = price
+    
+        else:    
+            price_with_discount = price - (price * discount / 100)
+    
+        return(price_with_discount)
     except(TypeError, ValueError):
         print('Введены неверные параметры')
-        max_discount = 0
-
-    
-    
-    
-    if max_discount > 99:
-        raise ValueError('Максимальная скидка не может быть больше 99%')
-
-    if discount >= max_discount or 'iphone' in name.lower() or not name:
-        price_with_discount = price
-    
-    else:    
-        price_with_discount = price - (price * discount / 100)
-    
-    return(price_with_discount)
-
 
         
              
