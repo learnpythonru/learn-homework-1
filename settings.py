b13f0d51d75bc10a826d4833fa2554b34517d117
Sergey Kurokhtin <don.sergio.q@gmail.com>
2021-05-20 23:47:48 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-# System settings
-API_KEY = "1839529646:AAFPOmGQeolEW_Nqf3GLzJFy1ciuKRH3q_E"
-PROXY_URL = "socks5://t1.learn.python.ru:1080"
-PROXY_USERNAME = "learn"
-PROXY_PASSWORD = "python"
-
-# Application settings
-DEFAULT_OBSERVER_CITY = 'Moscow'
