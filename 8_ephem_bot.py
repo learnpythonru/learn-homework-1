d7e5fd3a138f2aa5d1b1624ecb99aebd5022eb7e
jirotakeo <jirotakeo@gmail.com>
2021-05-28 18:16:00 +0300

    add /cities, /next_full_moon, /calc, /wordcount

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -14,13 +14,17 @@
 """
 import logging
 import ephem
+import numexpr as ne
+from random import randint
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from datetime import date
+from datetime import date, datetime
 from settings import TOKEN, PROXY
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
+
+date_now = date.today()
 
 
 def greet_user(update, context):
@@ -37,10 +41,28 @@ def talk_to_me(update, context):
 
 def in_which_constellation(update, context):
     user_text = update.message.text.split()[1].capitalize()
-    date_now = date.today()
     planet = getattr(ephem, user_text)
     constellation = ephem.constellation(planet(date_now))[1]
     update.message.reply_text(f'Планета {user_text} в созвездии {constellation}')
+
+
+def next_full_moon(update, context):
+    try:
+        user_text = update.message.text.split()[1]
+        user_date = datetime.strptime(user_text, '%Y-%m-%d')
+        update.message.reply_text(f'Следующее полнолуние {ephem.next_full_moon(user_date)}')
+    except IndexError:
+        update.message.reply_text(f'Следующее полнолуние {ephem.next_full_moon(date_now)}')
+
+
+def calculate(update, context):
+    user_text = update.message.text.split(' ')
+    str_expr = ''.join(user_text[1:])
+    try:
+        result = ne.evaluate(str_expr)
+    except ZeroDivisionError:
+        update.message.reply_text('Нельзя делить на ноль')
+    update.message.reply_text(f'{result}')
 
 
 def helper(update, context):
@@ -49,12 +71,71 @@ def helper(update, context):
                               '\nSaturn, Uranus, Venus, Jupiter')
 
 
+def create_cities_list():
+    with open('goroda.csv', 'r', encoding='utf-8') as f:
+        li = []
+        for i in f:
+            li.append(i.split(',')[0])
+        return li
+
+
+cities_list = create_cities_list()
+last_letter_bot_city = '0'
+
+
+def cities(update, context):
+    user_city = update.message.text.split()[1].capitalize()
+    global last_letter_bot_city
+    global cities_list
+    if user_city.lower() == 'stop':
+        cities_list = create_cities_list()
+        last_letter_bot_city = '0'
+        update.message.reply_text('Игра окончена!')
+    elif (user_city in cities_list and user_city[0] == last_letter_bot_city.upper()) or last_letter_bot_city == '0':
+        last_letter_user_city = user_city[-1]
+        if last_letter_user_city == 'й':
+            last_letter_user_city = 'и'
+        elif last_letter_user_city in ['ь', 'ы']:
+            last_letter_user_city = user_city[-2]
+        # update.message.reply_text(f'Тебе на "{last_letter_bot_city.upper()}"')
+        cities_list.remove(user_city)
+        li_lit = []
+        for i in cities_list:
+            if i[0] == last_letter_user_city.upper():
+                li_lit.append(i)
+        print(li_lit)
+        bot_city = li_lit[randint(0, len(li_lit))]
+        update.message.reply_text(bot_city)
+        last_letter_bot_city = bot_city[-1]
+        if last_letter_bot_city == 'й':
+            last_letter_bot_city = 'и'
+        elif last_letter_bot_city in ['ь', 'ы']:
+            last_letter_bot_city = bot_city[-2]
+        update.message.reply_text(f'Тебе на "{last_letter_bot_city.upper()}"')
+    else:
+        update.message.reply_text(f'Неа, тебе на "{last_letter_bot_city.upper()}"')
+
+
+def wordcount(update, context):
+    user_text = update.message.text.split(' ')[1:]
+    print(user_text)
+    if not user_text:
+        update.message.reply_text('это что такое? пустая строка? и что прикажешь считать?')
+    else:
+        update.message.reply_text(f'{len(user_text)} слова')
+# /wordcount 1234567 2345678 @#$%ˆ&*  - подумать как сделать проверку
+
+
 def main():
     mybot = Updater(TOKEN, request_kwargs=PROXY, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", in_which_constellation))
     dp.add_handler(CommandHandler("help", helper))
+    dp.add_handler(CommandHandler('wordcount', wordcount))
+    dp.add_handler(CommandHandler('next_full_moon', next_full_moon))
+    dp.add_handler(CommandHandler('cities', cities))
+    dp.add_handler(CommandHandler('calc', calculate))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
