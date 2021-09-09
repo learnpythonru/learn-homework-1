36533fb07d1e4a41cf5f468fdecc450bc3feb939
Valentine Makarov <octopulus96@gmail.com>
2021-09-09 21:33:18 +0300

    Added a loop while and error processing

--- 6_exception1.py
+++ 6_exception1.py
@@ -4,17 +4,22 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию hello_user() из задания while1, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
+* Перепишите функцию hello_user() из задания while1, чтобы она
+  перехватывала KeyboardInterrupt, писала пользователю "Пока!"
   и завершала работу при помощи оператора break
-    
+
 """
 
 def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
-    
+    user_say = None
+    try:
+        while user_say != 'Хорошо': #Выполнять цикл пока user_say не станет 'Хорошо'
+            user_say = input('Как дела?\n') #\n переход на следующую строку
+    except KeyboardInterrupt:
+            print('Пока')
+
 if __name__ == "__main__":
     hello_user()
