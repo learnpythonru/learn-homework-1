218678223d52f820eb6095989d3abcf7976d08a4
Evgenij Karpov <gedrip87@gmail.com>
2021-09-09 15:23:42 +0300

    rewrite function

--- 2_if2.py
+++ 2_if2.py
@@ -16,16 +16,14 @@
 """
 
 def compare_strings(string1, string2):
-  if string1 != str(string1) or string2 != str(string2):
+  if not(isinstance(string1,str) and isinstance(string2,str)):
     return 0
-  else:
-    if string1 == string2:
-      return 1
-    else:
-      if len(string1) > len(string2):
-        return 2
-      elif string2 == "learn":
-        return 3
+  elif string1 == string2:
+    return 1
+  elif string2 == 'learn':
+    return 3  
+  elif len(string1) > len(string2):
+    return 2
 
 
 def main():
