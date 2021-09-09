63037b56d684162065f72910224845ce856c3ea3
Valentine Makarov <octopulus96@gmail.com>
2021-09-09 20:31:06 +0300

    added function with class_score

--- 3_for.py
+++ 3_for.py
@@ -4,11 +4,27 @@
 
 Цикл for: Оценки
 
-* Создать список из словарей с оценками учеников разных классов 
+* Создать список из словарей с оценками учеников разных классов
   школы вида [{'school_class': '4a', 'scores': [3,4,4,5,2]}, ...]
 * Посчитать и вывести средний балл по всей школе.
 * Посчитать и вывести средний балл по каждому классу.
 """
+groups = [{'school_class' : '4A', 'scores' : [5,3,4,5,5]},
+    {'school_class' : '4B', 'scores' : [4,2,1,2,5]},
+    {'school_class' : '4C', 'scores' : [4,5,5,2,2]}]
+
+def class_score():
+  total_score = 0
+  for group in groups:
+        name = group['school_class']
+        scores = group['scores']
+        average_group_rating = (sum(scores))/(len(scores))
+        print(f'Средний бал класса {name}: {average_group_rating}')
+        for score in group['scores']:
+            total_score += score
+            average_school_rating = total_score/(len(scores)*len(groups))
+
+  print(f'Средний бал всей школы {average_school_rating}')
 
 def main():
     """
@@ -16,6 +32,6 @@ def main():
     В ней надо заменить pass на ваш код
     """
     pass
-    
+
 if __name__ == "__main__":
     main()
