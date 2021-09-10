0f65bac8ec640a68354b29c3fc0fde4f361bf4e0
ekaterina.kim <ekaterina.kim@diagnocat.com>
2021-09-10 21:36:42 +0300

    fixes

--- 3_for.py
+++ 3_for.py
@@ -9,97 +9,65 @@
 * Посчитать и вывести средний балл по всей школе.
 * Посчитать и вывести средний балл по каждому классу.
 """
-
-# scores1 = [
-#   {'school_class': '1a', 
-#    'scores': [
-#     5,4,2,2,5
-#   ]},
-#   {'school_class': '2a', 
-#    'scores': [
-#     4,5,3,2,5
-#   ]},
-#   {'school_class': '3a', 
-#    'scores': [
-#     3,5,3,2,5
-#   ]},
-#   {'school_class': '4a', 
-#    'scores': [
-#     5,5,4,3,5
-#   ]},
-#   {'school_class': '5a', 
-#    'scores': [
-#     2,2,3,4,5
-#   ]},
-# ]
-
-# the average value is 5 there - for check
-# scores1 = [
-#   {'school_class': '1a', 
-#    'scores': [
-#     5,5
-#   ]},
-#   {'school_class': '2a', 
-#    'scores': [
-#     5,5
-#    ]},
-#   {'school_class': '3a', 
-#    'scores': [
-#     5,5
-#    ]},
-#   {'school_class': '4a', 
-#    'scores': [
-#     5,5
-#    ]},
-#   {'school_class': '5a', 
-#    'scores': [
-#     5,5
-#    ]},
-# ]
-
+from rich import print
 
 scores1 = [
   {'school_class': '1a', 
    'scores': [
-    5,4,5,4
+    5,4,2,2,5
   ]},
   {'school_class': '2a', 
    'scores': [
-    5,5,5,5,5
-  ]}
+    4,5,3,2,5
+  ]},
+  {'school_class': '3a', 
+   'scores': [
+    3,5,3,2,5
+  ]},
+  {'school_class': '4a', 
+   'scores': [
+    5,5,4,3,5
+  ]},
+  {'school_class': '5a', 
+   'scores': [
+    2,2,3,4,5
+  ]},
 ]
+
+# scores1 = [
+#   {'school_class': '1a', 
+#    'scores': [
+#     5,4,5,4
+#   ]},
+#   {'school_class': '2a', 
+#    'scores': [
+#     5,5,5,5,5
+#   ]}
+# ]
     
-# this is the custom class for pushing things in list
-class List(list):
-    def push(self, x):
-        self.append(x)
-
-avg_class_scores = List()
-
-def get_avg_class_score(scores, avg_class_scores = None):
-  # пчм мы не можем создать массив для хранения средних оценок внутри функции?
-  avg_class_scores = List()
-
-  for score in scores:
-    avg_class_scores.push((sum(score['scores']) / len(score['scores'])))
+def get_avg_class_score(groups):
+  avg_scores = {}
+  for group in groups:
+    name = group['school_class'] 
+    scores = group['scores']
+    avg_scores[name] = sum(scores) / len(scores)
     
-    # changed brakets but still don'r work
-    # return (f"The average score of class: {score['school_class']} is { sum(score['scores']) / len(score['scores'])}")
-  
-  print(f'The average class scores are { avg_class_scores }')
-  return(avg_class_scores)
+  return avg_scores
      
-def get_avg_school_score(scores):
-  # why cannot we get avg of average class scores?
+def get_avg_school_score(groups): 
+  total_score = 0
+  total_count = 0
   
-  # scores = (get_avg_class_score(scores1))
-  print(scores)
-  # print(sum(scores))
-  # print(len(scores))
-  # return(f'The average score within school is: {sum(scores) / len(scores)}')
+  for group in groups:
+    scores = group['scores']
+    total_score += sum(scores)
+    total_count += len(scores)
+  
+  return total_score / total_count
 
 def main():
+    print(get_avg_class_score(scores1))
     print(get_avg_school_score(scores1))
     
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
