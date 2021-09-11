86affb79a969edf8c974d3b7ceab5e9e2b8bf3b7
Valentine Makarov <octopulus96@gmail.com>
2021-09-11 18:30:56 +0300

    Moved error handling into a separate function

--- 7_exception2.py
+++ 7_exception2.py
@@ -17,23 +17,31 @@ def discounted(price, discount, max_discount=20):
     """
     Замените pass на ваш код
     """
+
+    price = float(abs(price))
+    discount = float(abs(discount))
+    max_discount = int(abs(max_discount))
+    if max_discount >= 100:
+        raise ValueError('Слишком большая максимальная скидка')
+    if discount >= max_discount:
+        return price
+    else:
+        return price - (price * discount / 100)
+
+def print_discounted(price, discount, max_discount=20):
     try:
-        price = float(abs(price))
-        discount = float(abs(discount))
-        max_discount = int(abs(max_discount))
-        if max_discount >= 100:
-            raise ValueError('Слишком большая максимальная скидка')
-        if discount >= max_discount:
-            return price
-        else:
-            return price - (price * discount / 100)
-    except (TypeError, ValueError):
-        print('Type Error or Value Error')
+        print(discounted(price, discount, max_discount))
+    except (TypeError, ValueError) as err:
+        print(err)
+
+def main():
+
+    print_discounted(100, 2)
+    print_discounted(100, "3")
+    print_discounted("100", "4.5")
+    print_discounted("five", 5)
+    print_discounted("сто", "десять")
+    print_discounted(100.0, 5, "10")
 
 if __name__ == "__main__":
-    print(discounted(100, 2))
-    print(discounted(100, "3"))
-    print(discounted("100", "4.5"))
-    print(discounted("five", 5))
-    print(discounted("сто", "десять"))
-    print(discounted(100.0, 5, "10"))
+    main()
