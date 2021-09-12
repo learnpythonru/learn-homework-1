5c40bc171a7ca77693d9404aa942c16f4377ca50
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-09-12 13:59:56 +0300

    1. обновил форматирование 2. разбил длинную строку на 2

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -36,7 +36,8 @@ PROXY = {
 
 
 def greet_user(update, context):
-    text = '''Это астрономический бот! Введите /planet <planet> и узнаете в каком созвездии расположена планета сегодня! Например /planet mars'''
+    text = '''Это астрономический бот! Введите /planet <planet> и узнаете\
+     в каком созвездии расположена планета сегодня! Например /planet mars'''
     update.message.reply_text(text)
 
 
@@ -45,24 +46,25 @@ def talk_to_me(update, context):
     print(user_text)
     update.message.reply_text(user_text)
 
+
 def get_constellation(update, context):
     planets = {
-        "mercury":ephem.Mercury,
-        "venus":ephem.Venus,
-        "mars":ephem.Mars,
-        "jupiter":ephem.Jupiter,
-        "saturn":ephem.Saturn,
-        "uranus":ephem.Uranus,
-        "neptune":ephem.Neptune
+        "mercury": ephem.Mercury,
+        "venus": ephem.Venus,
+        "mars": ephem.Mars,
+        "jupiter": ephem.Jupiter,
+        "saturn": ephem.Saturn,
+        "uranus": ephem.Uranus,
+        "neptune": ephem.Neptune
     }
     try:
         planet = update.message.text.split()[1].lower()
     except IndexError:
         update.message.reply_text("введите планету в формате /planet <planet>")
-    
+
     if planet not in planets:
         update.message.reply_text("неизвестная планета")
-    
+
     today_planet = planets[planet](datetime.today().strftime("%Y/%m/%d"))
     constellation = ephem.constellation(today_planet)
     update.message.reply_text(constellation)
