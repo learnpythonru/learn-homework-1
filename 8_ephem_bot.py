9e58e129b1b296b06f71df4d3f5071a3a5507680
2281 <askarbin@rambler.ru>
2021-09-16 13:16:31 +0300

    add new feature. Game /cities

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -31,6 +31,8 @@ PROXY = {
     }
 }
 
+current_game_known_cities = ['Москва', 'Альметьевск', 'Киров', 'Волгоград', 'Дзержинск']
+
 def greet_user(update, context):
     text = 'Вызван /start'
     print(text)
@@ -47,11 +49,10 @@ def wordcount(update, context):
     user_text = user_text.strip('/wordcount ').split()
     text = f'{len(user_text)} слова'
     if len(user_text) == 0:
-        text = 'Нет слов'
-        print("Нет слов")
-    print(text)
-    update.message.reply_text(text)
+        text = 'Нет ни слов, ни чисел даже'
+        print(text)
 
+    update.message.reply_text(text)
 
 def where_is_planet(update, context):
     today = time.strftime('%Y/%m/%d')
@@ -68,13 +69,54 @@ def where_is_planet(update, context):
         print(constellation)
         update.message.reply_text(constellation)
 
+def next_full_moon(update, context):
+    user_text = update.message.text
+    print(user_text)
+    today = (user_text.split()[1])
+    full_moon_date = ephem.next_full_moon(today)
+    full_moon_date = ephem.date(full_moon_date + ephem.hour)
+    print(full_moon_date)
+    update.message.reply_text(f'Дата и время следующего полнолуния: {full_moon_date}')
+
+def game_cities(update, context):
+    #known_cities = ['Москва', 'Альметьевск', 'Киров', 'Волгоград', 'Дзержинск']
+    #current_game_known_cities = ['Москва', 'Альметьевск', 'Киров', 'Волгоград', 'Дзержинск']
+    current_game_called_cities = []
+    user_text = update.message.text
+
+    city = user_text.split()[1]
+    print(city)
+
+    if city in current_game_called_cities:
+        return update.message.reply_text(f'Баян, уже было')
+
+    current_game_known_cities.remove(city)
+    current_game_called_cities.append(city)
+
+    for answer_city in current_game_known_cities:
+        if answer_city[0].lower() == city[-1].lower():
+            update.message.reply_text(f'{answer_city}. Твой ход')
+        update.message.reply_text(f'Больше не знаю городов на {city[-1]}. Надо бы узнать. Пиши /cities Заново и начнём сначала. ')
+
+    if current_game_known_cities == []:
+        update.message.reply_text(f'Мы больше не знаем никаких городов. Напиши город Заново и начнём сначала')
+
+    if user_text.split()[1] == 'Заново':
+        return update.message.reply_text(f'Начинаем заново')
+
+
+
 def main():
     #mybot = Updater(config.TOKEN, request_kwargs=PROXY, use_context=True)
     mybot = Updater(config.TOKEN, use_context=True)
     dp = mybot.dispatcher
+
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler('planet', where_is_planet))
+    dp.add_handler(CommandHandler('next_full_moon', next_full_moon))
+    dp.add_handler(CommandHandler('cities', game_cities))
     dp.add_handler(CommandHandler('wordcount', wordcount))
+
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
