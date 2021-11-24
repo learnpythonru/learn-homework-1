a41cb639a108248b54796d0fd033dfb3dac9642e
Yan Tuzovskiy <yantuz2005@gmail.com>
2021-11-24 11:35:11 +0300

    А это было просто

--- 4_while1.py
+++ 4_while1.py
@@ -4,18 +4,20 @@
 
 Цикл while: hello_user
 
-* Напишите функцию hello_user(), которая с помощью функции input() спрашивает 
+* Напишите функцию hello_user(), которая с помощью функции input() спрашивает
   пользователя “Как дела?”, пока он не ответит “Хорошо”
-   
+
 """
 
 
-def hello_user():
+def hello_user(how = None):
     """
     Замените pass на ваш код
     """
-    pass
+    while how != 'Хорошо':
+        print('Привет, как дела?')
+        how = str(input())
 
-    
+
 if __name__ == "__main__":
     hello_user()
