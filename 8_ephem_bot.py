8a37a17b128e2dd8a6745379f7be29f391d33079
Daria Veselkova <daria.veselkova@yandex.ru>
2021-11-27 00:15:15 +0300

    Task 8 done

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -13,8 +13,10 @@
 
 """
 import logging
-
+import ephem
+import settings
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -22,33 +24,48 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 
 PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
+        'username': settings.PROXY_USERNAME,
+        'password': settings.PROXY_PASSWORD
     }
-}
+  }
 
 
 def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+    print("Вызван /start")
+    #print(update)
+    update.message.reply_text("Привет! Ты вызвал команду /start. Давай начнем!")
 
 
 def talk_to_me(update, context):
     user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(text)
+    print("Пользователь ввел: ", user_text)
+    update.message.reply_text(user_text)
+
+
+def planet_constellation(update, context):   
+    user_text = update.message.text.split()
+    user_body = user_text[1].lower().capitalize()
+    
+    body_class = getattr(ephem, user_body, None)
+    if body_class:
+        body = body_class('2021/11/26')                                 
+        constellation = ephem.constellation(body)
+        update.message.reply_text(constellation)
+    else:
+        update.message.reply_text(f'Объект с названием {user_body} не найден')
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, request_kwargs=PROXY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet_constellation))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
+    logging.info("Bot started")
     mybot.start_polling()
     mybot.idle()
 
