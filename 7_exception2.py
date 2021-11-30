3ebfe26a0c6b30bc016a3e38d0c01181b22f588e
gr4mka <88055914+gr4mka@users.noreply.github.com>
2021-11-30 23:41:13 +0300

    7е задание

--- 7_exception2.py
+++ 7_exception2.py
@@ -11,45 +11,25 @@
   а третий - к целому при помощи int() и перехватывать исключения
   ValueError и TypeError, если приведение типов не сработало.
 
-# lesson 6 (функции)
-def discounted(price, discount, max_discount=20):
-    price = abs(price)
-    discount = abs(discount)
-    max_discount = abs(max_discount)
-    if max_discount >= 100:
-        raise ValueError('Слишком большая максимальная скидка')
-    if discount >= max_discount:
-        return price
-    else:
-         return price - (price * discount / 100)
-product = {'name': 'Samsung Galaxy S21', 'price': 50000.0, 'discount': 5}
-product['price_discounted'] = discounted(product['price'], product['discount'])
-print(product)
-
-#Задание 1
-def get_summ(one, two, delimiter ='&'):
-    summ = str.upper(one) + delimiter + str.upper(two) #сложение с переводом строки в заглавные буквы(верхний регистр), либо использовать str.title для заглавных первых букв
-    print (summ)
-data = {'one': 'learn', 'two': 'python'} # словарь с исходными данными для функции
-result = get_summ(data['one'], data['two']) # выполнение функции для данного списка
-
-#Задание 2
-def format_price(price):
-    integer = str(int(price))
-    message = f'Цена: {integer} руб.'
-    print (message)
-price = 56.24
-format_price(price)
-
-
-
 """
 
+# from lesson 6 (функции)
 def discounted(price, discount, max_discount=20):
-    """
-    Замените pass на ваш код
-    """
-    pass
+    try: # добавляем к стандартному переводу типов
+        price = abs(price)
+        discount = abs(discount)
+        max_discount = abs(max_discount)
+        if max_discount >= 100:
+            raise ValueError('Слишком большая максимальная скидка')
+        if discount >= max_discount:
+            return price
+        else:
+            return price - (price * discount / 100)
+#product = {'name': 'Samsung Galaxy S21', 'price': 50000.0, 'discount': 5}
+#product['price_discounted'] = discounted(product['price'], product['discount'])
+#print(product)
+    except (ValueError, TypeError): # перехват исключений
+        print('Некорректные данные') # 5 из 6
     
 if __name__ == "__main__":
     print(discounted(100, 2))
