3b29d85d49bcba58f807b98e8354866656443298
Valexandrin <alex-vadim@mail.ru>
2022-02-15 18:57:56 +0300

    Task 4 is ready

--- 4_while1.py
+++ 4_while1.py
@@ -10,11 +10,18 @@
 """
 
 
+from ast import While
+
+
 def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
+
+    while True:
+        user_say = input('Как дела? ')
+        if user_say == 'Хорошо':
+            break
 
     
 if __name__ == "__main__":
