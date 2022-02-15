0340de1dc9afe6d3c511e53055f28cb66b9cc8bc
Valexandrin <alex-vadim@mail.ru>
2022-02-15 18:39:10 +0300

    Task 3 is ready

--- 3_for.py
+++ 3_for.py
@@ -16,12 +16,37 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
+def sum_sales(sales):
+    sales_sum = 0
+    for item in sales:
+        sales_sum += item
+    return sales_sum
+
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+
+    phones = [
+        {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+        {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+        {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+    ]
+
+    total_sales = 0    
+    avg_total_sales = 0
+    for product in phones:
+        sales_by_product = sum_sales(product['items_sold'])        
+        avg_sales = round(sales_by_product / len(product['items_sold']), 2)
+        print(f'{product["product"]} - Sales: {sales_by_product}. Average sales: {avg_sales}')
+        total_sales += sales_by_product
+        avg_total_sales += round(total_sales / len(phones), 2)
+
+    print(f'Total sales: {total_sales}')
+    print(f'Average total sales: {avg_total_sales}')
+
     
 if __name__ == "__main__":
     main()
