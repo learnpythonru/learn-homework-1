596be9b377410460ac056faf958631645b3ab0b7
Ilya Galaktionov <igalaktionov1@gmail.com>
2022-02-17 21:58:12 +0300

    Check please

--- 6_exception1.py
+++ 6_exception1.py
@@ -4,17 +4,24 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию hello_user() из задания while1, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
+* Перепишите функцию hello_user() из задания while1, чтобы она
+  перехватывала KeyboardInterrupt, писала пользователю "Пока!"
   и завершала работу при помощи оператора break
-    
+
 """
 
+
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+    try:
+        user_answer = input('Как дела?: ')
+        user_answer = user_answer.lower()
+        while user_answer == 'хорошо':
+            break
+        else:
+            return hello_user()
+    except KeyboardInterrupt:
+        print('\nПока!')
+
+
 if __name__ == "__main__":
     hello_user()
