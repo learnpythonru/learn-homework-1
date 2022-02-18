36243025e0e94adeaf0cc7c384d0a6e2c49ec545
Aleksandr Sadikov <sadikov.sasha91@mail.ru>
2022-02-18 16:05:48 +0300

    Исправил код второй задачи if

--- 2_if2.2.py
+++ 2_if2.2.py
@@ -1,21 +1,40 @@
-
 def main():
-
     def strings(first_string, second_string):
-        if isinstance(first_string, str) and isinstance(second_string, str):
-            return 'Строка'
-        elif not isinstance(first_string, str) and not isinstance(second_string, str):
+        if first_string and second_string:
+            if first_string == second_string:
+                return '1'
+            elif first_string != second_string and len(first_string) > len(second_string):
+                return '2'
+            elif first_string != second_string and second_string =='learn':
+                return '3'
+            elif isinstance(first_string, str) and isinstance(second_string, str):
+                return 'Строка'
+        else:
             return '0'
-        elif first_string == second_string:
-            return '1'
-        elif first_string != second_string and len(first_string) > len(second_string):
-            return '2'
-        elif first_string != second_string and second_string =='learn':
-            return '3'
 
     print(strings(first_string = 'Home', second_string = 'learn'))
-    print(strings(first_string = 1, second_string = 2))
-    print(strings(first_string = , second_string = ))
+    print(strings(first_string = 'Home', second_string = 'Home'))
+    print(strings(first_string = 'Home', second_string = 'Apple'))
+    print(strings(first_string = True, second_string = False))
+
+
+
 
 if __name__ == "__main__":
-    main()
+    main()           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
