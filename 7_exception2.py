9c09d83ffc53f4e100176da7b32816728fbf4299
Aleksandr Sadikov <sadikov.sasha91@mail.ru>
2022-02-20 14:53:55 +0300

    Задание 7 Исключения 2, 2 лишних None

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,16 +13,31 @@
     
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
+def discounted(price, discount, max_discount = 20):
+    try:
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = abs(int(max_discount))
+    except (ValueError):
+        print('Неверный ввод данных')
+        return 
+    if max_discount >= 100:
+        raise ValueError('Максимальная скидка не должна быть больше 100')
+    if discount >= max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price - (price * discount / 100)
+        return price_with_discount
+  
+     
     
 if __name__ == "__main__":
-    print(discounted(100, 2))
-    print(discounted(100, "3"))
-    print(discounted("100", "4.5"))
-    print(discounted("five", 5))
-    print(discounted("сто", "десять"))
-    print(discounted(100.0, 5, "10"))
+    
+        print(discounted(100, 2))
+        print(discounted(100, "3"))
+        print(discounted("100", "4.5"))
+        print(discounted("five", 5))
+        print(discounted("сто", "десять"))
+        print(discounted(100.0, 5, "10"))
+    
+    
