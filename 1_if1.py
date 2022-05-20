8052d5e666eb8941b0a78b493f1a875b8fdc1308
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-20 09:20:50 +0800

    Добавил func stage_of_life, Выполнил задание

--- 1_if1.py
+++ 1_if1.py
@@ -28,13 +28,23 @@ def get_input() -> int:
     return age
 
 
+def stage_of_life(age: int) -> str:
+    if age <= 6: return 'Учиться в детском саду'
+    if age >= 7 and age <= 17: return 'Учиться в школе'
+    if age >= 18 and age <= 24: return 'Учиться в ВУЗе'
+    if age >= 25 and age <= 64: return 'Работает'
+    if age >= 65: return 'Пенсионер'
+
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-
+    
+    age = get_input()
+    work = stage_of_life(age)
+    print(work)
 
 if __name__ == "__main__":
     main()
