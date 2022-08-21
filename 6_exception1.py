ecd420dfe49146f3958977feaf609a8b0fb42e87
ILya Ivanov <ivanovrocket@gmail.com>
2022-08-21 16:42:19 -0400

    second part home tasks

--- 6_exception1.py
+++ 6_exception1.py
@@ -11,10 +11,14 @@
 """
 
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+	try:
+		while True:
+			user_said = input('Как дела? ')
+			if user_said == 'Хорошо':
+				break
+	except KeyboardInterrupt:
+		print('Пока!')
+
+
 if __name__ == "__main__":
     hello_user()
--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -13,44 +13,52 @@
 
 """
 import logging
-
+import ephem
+import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log')
+import settings
 
+logging.basicConfig(filename='bot.log', level=logging.INFO)
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
-
+PROXY = {'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
 
 def greet_user(update, context):
-    text = 'Вызван /start'
+    print('Вызван /start')
+    print(update)
+    update.message.reply_text('Здравствую, пользователь!')
+
+def tolk_to_me(update, context):
+    text = update.message.text
     print(text)
     update.message.reply_text(text)
 
-
-def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(text)
+def get_planet(update, context):
+    print('Вызванна команда /planet')
+    print(update)
+    text = update.message.text.split()
+    planet = text[1]
+    date = datetime.datetime.now()
+    if planet.lower() == 'mars':
+        m = ephem.Mars(date)
+        update.message.reply_text(ephem.constellation(m))
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True) #request_kwargs=PROXY
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', get_planet))
+    dp.add_handler(MessageHandler(Filters.text, tolk_to_me))
 
+    logging.info('Бот стартовал')
     mybot.start_polling()
     mybot.idle()
+
+if  __name__ == '__main__':
+    main()
 
 
 if __name__ == "__main__":
