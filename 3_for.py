9167e8ec95a8cdfe64fe7e82c7bea0e483be4d44
Tamerlan Kasaev <tamerkasaev@gmail.com>
2022-08-26 14:46:11 +0300

    3_for made

--- 3_for.py
+++ 3_for.py
@@ -21,7 +21,14 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+  phone_list =[
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+    {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+    ]
+  
+  for product in phone_list:
+    for line in product:
     
 if __name__ == "__main__":
     main()
