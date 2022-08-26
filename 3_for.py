05cd36b7d562521902677725cdd62c1aee3d2bd4
Tamerlan Kasaev <tamerkasaev@gmail.com>
2022-08-26 20:13:16 +0300

    Correction

--- 3_for.py
+++ 3_for.py
@@ -21,15 +21,15 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-  phone_list =[
+  phone_sales =[
     {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
     ]
   
-  for product in phone_list:
-    for line in product:
-    
+  for product in phone_sales:
+    for item in product:
+      for sales in item:
 pass
     
 if __name__ == "__main__":
