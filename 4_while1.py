a5a12505466f937e0ecbfba80d4f84ee2a82d9c8
Maria Presnukhina <igomary@gmail.com>
2022-09-06 21:15:57 +0300

    4th done

--- 4_while1.py
+++ 4_while1.py
@@ -14,7 +14,10 @@ def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
+    while True:
+      answer = input("Как дела? ")
+      if answer == "Хорошо":
+        break
 
     
 if __name__ == "__main__":
