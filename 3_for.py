bed72e3a1d9933ee516061368612a1af1d22c9eb
Maria Presnukhina <igomary@gmail.com>
2022-09-06 21:13:14 +0300

    3rd done

--- 3_for.py
+++ 3_for.py
@@ -15,13 +15,37 @@
 * Посчитать и вывести суммарное количество продаж всех товаров
 * Посчитать и вывести среднее количество продаж всех товаров
 """
+def sum_sold(sold):
+  sum = 0
+  for each in sold:
+    sum += each
+  return sum
+
+def avg_sold(sold):
+  return sum_sold(sold)/len(sold)
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    products = [
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+    {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+    ]
+    sum_all = 0
+    quant = 0
+    for each in products:
+      sum = sum_sold(each['items_sold'])
+      avg = avg_sold(each['items_sold'])
+      print(f"Всего продано {each['product']}: {sum}")
+      print(f"В среднем продавалось {each['product']}: {avg}")
+      sum_all += sum
+      quant += len(each['items_sold'])
+
+    print(f'Всего продано {sum} телефонов')
+    print(f'Средне число проданных: {sum_all/quant}')
     
 if __name__ == "__main__":
     main()
