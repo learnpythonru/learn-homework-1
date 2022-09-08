d208ea3d64a079b3cdd3c2f115c3a19f8dfca184
Dmitry <d.kucherov98@yandex.ru>
2022-09-08 13:16:56 +0500

    Сделал третье задание

--- 3_for.py
+++ 3_for.py
@@ -6,7 +6,7 @@
 
 * Дан список словарей с данными по колличеству проданных телефонов
   [
-    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
@@ -16,12 +16,24 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
-if __name__ == "__main__":
-    main()
+sales = [
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
+    {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+    {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+]
+
+all_sum = 0
+all_len = 0
+for phone in sales:
+    sum_score = sum(phone['items_sold'])
+    print(f"Суммарное количество продаж {phone['product']}: {sum_score}")
+
+    avg_sum = sum_score // len(phone['items_sold'])
+    print(f"Среднее количество продаж {phone['product']}: {avg_sum}")
+
+    all_len += len(phone['items_sold'])
+    all_sum += sum_score
+
+    avg_all_sum = all_sum // all_len
+print(f"Среднее количество продаж всех товаров: {avg_all_sum}")
+print(f"Суммарное количество продаж всех товаров: {all_sum}")
