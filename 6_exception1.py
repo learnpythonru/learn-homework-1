49120bcb972e6dbaf6ef7759ac81adff4775546b
Dmitry <d.kucherov98@yandex.ru>
2022-09-11 17:33:39 +0500

    Сделал 6 задание

--- 6_exception1.py
+++ 6_exception1.py
@@ -4,17 +4,21 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию hello_user() из задания while1, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
+* Перепишите функцию hello_user() из задания while1, чтобы она
+  перехватывала KeyboardInterrupt, писала пользователю "Пока!"
   и завершала работу при помощи оператора break
-    
+
 """
 
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+    while True:
+        try:
+            answer = input('Как дела? \n').capitalize()   # сделает первую введенную букву заглавной
+            if answer == 'Хорошо':
+                break
+        except KeyboardInterrupt:
+            print('Пока!')
+            break
+
 if __name__ == "__main__":
     hello_user()
