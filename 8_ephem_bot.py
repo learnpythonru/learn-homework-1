39e47dccfd93d0dbefd6c72e4512a3585b1cdafa
Stepanova Daria <videotakvideo@gmail.com>
2022-11-30 11:01:16 +0700

    bot is ready

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -13,44 +13,68 @@
 
 """
 import logging
+import ephem
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler
+from telegram.ext import MessageHandler, Filters
+from dotenv import load_dotenv
+import os
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+load_dotenv()
+API_KEY = os.getenv("API_KEY_PLANET_BOT")
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log')
+from datetime import datetime
 
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
+planet_aliases = {
+    "Марс": "Mars",
+    "Уран": "Uranus",
+    "Юпитер": "Jupiter",
+    "Меркурий": "Mercury",
+    "Сатурн": "Saturn",
+    "Нептун": "Neptune"
 }
 
 
-def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+def get_planet_constellation(planet_alias):
+    today = datetime.today().strftime('%Y/%m/%d')
 
+    planet_name = planet_aliases.get(planet_alias, planet_alias)
+    print(planet_name)
+
+    #planets
+    planet_attr = getattr(ephem, planet_name, None)
+    if not planet_attr:
+        return None
+
+    planet = planet_attr(today)
+    constellation = ephem.constellation(planet)
+    return constellation
+
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+def greet_user(update, context):
+    text = "Привет,  напиши название планеты в формате 'Марс'"
+    update.message.reply_text(text)
 
 def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(text)
+    constellation = get_planet_constellation(update.message.text)
+    if not constellation:
+        update.message.reply_text("Я тебя не понимаю. Напиши /start.")
+        return
+    update.message.reply_text(constellation[1] )
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
-
+    mybot = Updater(API_KEY
+, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
     mybot.start_polling()
     mybot.idle()
+
 
 
 if __name__ == "__main__":
