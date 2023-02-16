ed607945894a7d574c621e25106a7ef6ee341b55
Sergey Ilin <ilin_sergey90@mail.ru>
2023-02-16 07:01:12 +0300

    8 task complited

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,46 +12,5 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
-import logging
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log')
-
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
-
-
-def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
-
-
-def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(text)
-
-
-def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
-
-    dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
-    mybot.start_polling()
-    mybot.idle()
-
-
-if __name__ == "__main__":
-    main()
+# Бот живет в отдельном репозитории: https://github.com/IlinSergey/mybot
