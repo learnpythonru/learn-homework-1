d94fc35c8f8ffc58dc68e2fe3f3254172b2063b6
Sergey Ilin <ilin_sergey90@mail.ru>
2023-02-16 19:11:03 +0300

    delete one nesting level

--- 2_if2.py
+++ 2_if2.py
@@ -16,15 +16,14 @@
 """
 
 def main(string_1: str, string_2: str):
-  if not isinstance(string_1, str) or not isinstance(string_2, str):
-    return 0
-  else:
-    if string_1 == string_2:
-      return 1
+    if not isinstance(string_1, str) or not isinstance(string_2, str):
+        return 0
+    elif string_1 == string_2:
+        return 1
     elif string_1 != string_2 and string_2 == "learn":
-      return 3
+        return 3
     elif string_1 != string_2 and len(string_1) > len(string_2):
-      return 2   
+        return 2   
     
     
 if __name__ == "__main__":
