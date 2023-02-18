fa0bfff1a23c2b309666acec7122aa14b1e375b1
Vika Trofimova <vika_trofimova_1999@mail.ru>
2023-02-18 20:18:42 +0300

    Did the 6th task "Exception1"

--- 6_exception1.py
+++ 6_exception1.py
@@ -4,17 +4,28 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию hello_user() из задания while1, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
+* Перепишите функцию hello_user() из задания while1, чтобы она
+  перехватывала KeyboardInterrupt, писала пользователю "Пока!"
   и завершала работу при помощи оператора break
-    
+
 """
 
-def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+user_say = str(input('Как дела? '))
+
+def hello_user(user_say):
+
+    try:
+        while True:
+
+         if user_say.lower() == 'хорошо':
+            break
+
+        else:
+            user_say = input('Как дела? ')
+
+    except KeyboardInterrupt:
+        print('Пока!')
+
+
 if __name__ == "__main__":
-    hello_user()
+    hello_user(user_say)
