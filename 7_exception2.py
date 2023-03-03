84113d80c01ce9d47e17c38f7d8db6b289f6102d
Viktor Shishkalov <viktorshish@gmail.com>
2023-03-03 19:25:10 +0300

    Add exception

--- 7_exception2.py
+++ 7_exception2.py
@@ -14,15 +14,22 @@
 """
 
 def discounted(price, discount, max_discount=20):
-  price = abs(price)
-  discount = abs(discount)
-  max_discount = abs(max_discount)
-  if max_discount >= 100:
-      raise ValueError("Слишком большая максимальная скидка")
-  if discount >= max_discount:
-      return price
-  else:
-      return price - (price * discount / 100)
+    try:
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = abs(int(max_discount))
+        if max_discount >= 100:
+            raise ValueError("Слишком большая максимальная скидка")
+        if discount >= max_discount:
+            return price
+        else:
+            return price - (price * discount / 100)
+
+    except(ValueError):
+        print('Неправильное значение переменной')
+
+    except(TypeError):
+        print('Неправильный тип переменных')
 
 if __name__ == "__main__":
     print(discounted(100, 2))
@@ -30,4 +37,4 @@ if __name__ == "__main__":
     print(discounted("100", "4.5"))
     print(discounted("five", 5))
     print(discounted("сто", "десять"))
-    print(discounted(100.0, 5, "10"))
+    print(discounted(100.0, 5, [1, 2]))
