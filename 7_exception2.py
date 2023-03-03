91e6619f2888739738d85747a87825d1006b388a
Viktor Shishkalov <viktorshish@gmail.com>
2023-03-03 16:15:06 +0300

    Add def discounted()

--- 7_exception2.py
+++ 7_exception2.py
@@ -10,15 +10,20 @@
 * Первые два нужно приводить к вещественному числу при помощи float(),
   а третий - к целому при помощи int() и перехватывать исключения
   ValueError и TypeError, если приведение типов не сработало.
-    
+
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+def discounted(price, discount, max_discount=20):
+  price = abs(price)
+  discount = abs(discount)
+  max_discount = abs(max_discount)
+  if max_discount >= 100:
+      raise ValueError("Слишком большая максимальная скидка")
+  if discount >= max_discount:
+      return price
+  else:
+      return price - (price * discount / 100)
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
