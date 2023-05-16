35ebc24e4b75709cbb5c09b16e440e9e10bc9c20
leeroymk <nikekursk@gmail.com>
2023-05-16 18:48:10 +0300

    new changes

--- 7_exception2.py
+++ 7_exception2.py
@@ -1,29 +1,18 @@
-"""
-
-Домашнее задание №1
-
-Исключения: приведение типов
-
-* Перепишите функцию discounted(price, discount, max_discount=20)
-  из урока про функции так, чтобы она перехватывала исключения,
-  когда переданы некорректные аргументы.
-* Первые два нужно приводить к вещественному числу при помощи float(),
-  а третий - к целому при помощи int() и перехватывать исключения
-  ValueError и TypeError, если приведение типов не сработало.
-
-"""
-
-
 def discounted(price, discount, max_discount=20):
-    price = abs(price)
-    discount = abs(discount)
-    max_discount = abs(max_discount)
-    if max_discount >= 100:
-        raise ValueError('Слишком большая максимальная скидка')
-    if discount >= max_discount:
-        return price
+
+    try:
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = abs(int(max_discount))
+    except ValueError or TypeError:
+        return 'Неверное значение!'
     else:
-        return price - (price * discount / 100)
+        if max_discount >= 100:
+            raise ValueError('Слишком большая максимальная скидка')
+        if discount >= max_discount:
+            return price
+        else:
+            return price - (price * discount / 100)
 
 
 if __name__ == "__main__":
