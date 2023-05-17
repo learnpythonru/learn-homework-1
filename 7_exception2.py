b7a2b51d3131e98d4ac6ec714a01c687d75b8433
39r2d4 <telepuzik.blg@gmial.com>
2023-05-17 22:29:31 +0200

    Добавил решение для задания 7 + опечатки в задании 3

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,26 +13,59 @@
     
 """
 
-def discounted(price, discount, max_discount=20):
+def discounted(price=None, discount=None, max_discount=20):
+    """
+    
+    """
+    if (price and discount) is None:
+        print("Ввеите аргументы(цена и скидка)")
+        raise ValueError("Ввеите аргументы(цена и скидка)")
+    
     try:
-        price = abs(price)
-        discount = abs(discount)
-        max_discount = abs(max_discount)
-        if max_discount >= 100:
-            raise ValueError('Слишком большая максимальная скидка')
-        if discount >= max_discount:
-            return price
-        else:
-            return price - (price * discount / 100)
-    except(ValueError, TypeError):
-        print("Что-то пошло не так, проверьте значения аргументов!")
+        price = int(price)
+    except ValueError:
+        try:
+            price = float(price)
+        except ValueError:
+            print("Не верное значение цены")
+            raise ValueError("Не верное значение цены")
+
+    try:
+        discount = int(discount)
+    except ValueError:
+        try:
+            discount = float(discount)
+        except ValueError:
+            print("Не верное значение Скидки")
+            raise ValueError("Не верное значение Скидки")
+
+    try:
+        max_discount = int(max_discount)
+    except ValueError:
+        try:
+            max_discount = float(max_discount)
+        except ValueError:
+            print("Не верное значение максимальной скидки")
+            raise ValueError("Не верное значение максимальной скидки")
+
+    
+    price = abs(price)
+    discount = abs(discount)
+    max_discount = abs(max_discount)
+       
+    if max_discount >= 100:
+        raise ValueError('Слишком большая максимальная скидка')
+    if discount >= max_discount:
+        return price
+    else:
+        return price - (price * discount / 100)
 
     
 if __name__ == "__main__":
-    #print(discounted(100))
     print(discounted(100, 2))
-    print(discounted(100, "3"))
+    print(discounted(100, "3"))    
     print(discounted("100", "4.5"))
     print(discounted("five", 5))
     print(discounted("сто", "десять"))
     print(discounted(100.0, 5, "10"))
+
