622fd30b78e9a125bb49067e973749b20b0c3e76
БадриевА <zzdl4@bk.ru>
2023-05-18 11:06:29 +0300

    Done

--- 3_for.py
+++ 3_for.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 
 Домашнее задание №1
@@ -6,7 +8,7 @@
 
 * Дан список словарей с данными по колличеству проданных телефонов
   [
-    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
@@ -16,12 +18,42 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
+
+def sum_sales_for_each_product(lst):
+    for i in lst:
+        print(f'Cуммарное количество продаж товара: "{i["product"]}" составляет {sum(i["items_sold"])} шт.')
+
+
+def avg_sales_for_each_product(lst):
+    for i in lst:
+        print(f'Среднее количество продаж товара: "{i["product"]}" составляет '
+              f'{round(np.average(i["items_sold"]))} шт.')
+
+
+def sum_sales_for_all_product(lst):
+    print(f'Суммарное количество продаж товаров: "{", ".join([i["product"] for i in lst])}" составляет '
+          f'{sum([sum(i["items_sold"]) for i in lst])} шт.')
+
+
+def avg_sales_for_all_product(lst):
+    print(f'Среднее количество продаж товаров: "{", ".join([i["product"] for i in lst])}" составляет '
+          f'{round(np.average([np.average(i["items_sold"]) for i in lst]))} шт.')
+
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
+    sales = [
+        {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
+        {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+        {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+    ]
+    sum_sales_for_each_product(sales)
+    print('_' * 75)
+    avg_sales_for_each_product(sales)
+    print('_' * 75)
+    sum_sales_for_all_product(sales)
+    print('_' * 75)
+    avg_sales_for_all_product(sales)
+
+
 if __name__ == "__main__":
     main()
