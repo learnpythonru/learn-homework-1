fba9203f85ae2882a2c0c4ca012d7e647b1a0c07
БадриевА <zzdl4@bk.ru>
2023-05-18 12:45:56 +0300

    Done

--- 4_while1.py
+++ 4_while1.py
@@ -4,18 +4,17 @@
 
 Цикл while: hello_user
 
-* Напишите функцию hello_user(), которая с помощью функции input() спрашивает 
+* Напишите функцию hello_user(), которая с помощью функции input() спрашивает
   пользователя “Как дела?”, пока он не ответит “Хорошо”
-   
+
 """
 
 
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
+    while True:
+        if input('Как дела?\n').capitalize() == 'Хорошо':
+            break
 
-    
+
 if __name__ == "__main__":
     hello_user()
