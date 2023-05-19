d3154c9e847d9568a42388fe4d223ea90c18ec4e
Roman Paltsev <system256@ya.ru>
2023-05-19 22:07:15 +0300

    7_exception2 completed

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,11 +13,21 @@
     
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
+def discounted(price, discount, max_discount=20):
+    try:
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = abs(int(max_discount))
+    except (TypeError, ValueError):
+        return "Введите корректные значения"
+
+    if max_discount >= 100:
+         raise ValueError("Слишком большая максимальная скидка")
+    if discount >= max_discount:
+         return price
+    else:
+         return price - (price * discount / 100)
+
     
 if __name__ == "__main__":
     print(discounted(100, 2))
