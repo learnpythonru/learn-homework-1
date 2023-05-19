50de0a084787ecc3921d7bf2270091514d3a0239
Алмаз <zzdl4@bk.ru>
2023-05-19 07:28:03 +0300

    Done

--- 7_exception2.py
+++ 7_exception2.py
@@ -10,15 +10,36 @@
 * Первые два нужно приводить к вещественному числу при помощи float(),
   а третий - к целому при помощи int() и перехватывать исключения
   ValueError и TypeError, если приведение типов не сработало.
-    
-"""
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+"""
+import sys
+
+
+def discounted(price, discount, max_discount=20):
+    try:
+        price = abs(float(price))
+    except (ValueError, TypeError):
+        return f'Произошла ошибка {sys.exc_info()[0]}. Проверьте корректность введенной цены товара {price=}.'
+
+    try:
+        discount = abs(float(discount))
+    except (ValueError, TypeError):
+        return f'Произошла ошибка {sys.exc_info()[0]}. Проверьте корректность введенной скидки на товар {discount=}.'
+
+    try:
+        max_discount = abs(int(max_discount))
+    except (ValueError, TypeError):
+        return f'Произошла ошибка {sys.exc_info()[0]}. Проверьте корректность введенной максимальной ' \
+               f'скидки на товар {max_discount=}.'
+
+    if max_discount >= 100:
+        raise ValueError("Слишком большая максимальная скидка")
+    if discount >= max_discount:
+        return price
+    else:
+        return price - (price * discount / 100)
+
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
@@ -26,3 +47,5 @@ if __name__ == "__main__":
     print(discounted("five", 5))
     print(discounted("сто", "десять"))
     print(discounted(100.0, 5, "10"))
+    print(discounted(None, 5, "10.0"))
+    print(discounted(100, 5, None))
