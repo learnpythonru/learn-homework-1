2832226fc07394df525075b73b5108a2cd51c61b
Shestak Aleksandr <alxsh.usib@ya.ru>
2023-11-20 15:33:36 +0800

    5 задание

--- 5_while2.py
+++ 5_while2.py
@@ -1,3 +1,4 @@
+import urllib.request
 """
 
 Домашнее задание №1
@@ -14,14 +15,27 @@
     Программа: Программирую
     
 """
-
-questions_and_answers = {}
+def external_ip():
+    external_ip = str(urllib.request.urlopen('https://ident.me').read().decode('utf8')) #Обработать try/except
+    return external_ip
+ext_ip = external_ip()
+questions_and_answers = {"Как дела?": "Хорошо!", "Что делаешь?": "Программирую", "Какой у тебя ip?": f'{ext_ip}'}
+quest = ''.join(f'{k}'+',' for k in questions_and_answers.keys())
 
 def ask_user(answers_dict):
-    """
-    Замените pass на ваш код
-    """
-    pass
+    q = f'Возможные вопросы:\n{quest}'
+    print(q)
+    while True:
+        try:
+            questions = (input('Введите вопрос:')).lower()
+            a = questions_and_answers.get(questions.capitalize())
+            if a:
+                print(a)
+            else:
+                print(f'----Введите вопрос из списка!----\n{q}\nЧтобы выйти, нажмите Ctrl+C')
+                continue
+        except KeyboardInterrupt:
+            break
     
 if __name__ == "__main__":
     ask_user(questions_and_answers)
