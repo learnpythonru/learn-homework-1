bffc66022bd95e281b3d0edf625cb69e55007bf6
Dmitriy Kostarev <dmitriykostarev9987@gmail.com>
2024-02-21 23:28:32 +0300

    add solution 1_if1; fix after review 2_if2; add solution 3_for

--- 2_if2.py
+++ 2_if2.py
@@ -5,22 +5,38 @@
 Условный оператор: Сравнение строк
 
 * Написать функцию, которая принимает на вход две строки
-* Проверить, является ли то, что передано функции, строками. 
+* Проверить, является ли то, что передано функции, строками.
   Если нет - вернуть 0
 * Если строки одинаковые, вернуть 1
 * Если строки разные и первая длиннее, вернуть 2
 * Если строки разные и вторая строка 'learn', возвращает 3
-* Вызвать функцию несколько раз, передавая ей разные праметры 
+* Вызвать функцию несколько раз, передавая ей разные праметры
   и выводя на экран результаты
 
 """
+
+
+def check_two_strings(first_string: str, second_string: str) -> int:
+    if type(first_string) != str or type(second_string) != str:
+        return 0
+    if first_string == second_string:
+        return 1
+    if len(first_string) > len(second_string):
+        return 2
+    if first_string != second_string and second_string == 'learn':
+        return 3
+
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    print(check_two_strings('s', 5))
+    print(check_two_strings('s', 's'))
+    print(check_two_strings('spk', 's'))
+    print(check_two_strings('s', 'learn'))
+
+
 if __name__ == "__main__":
     main()
--- 3_for.py
+++ 3_for.py
@@ -6,7 +6,7 @@
 
 * Дан список словарей с данными по колличеству проданных телефонов
   [
-    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
@@ -16,12 +16,46 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
+product_list = [
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
+    {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+    {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+]
+
+
+def count_sold_items(items_sold: list) -> int:
+    count_items = 0
+    for item in items_sold:
+        count_items += item
+    return count_items
+
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    for product in product_list:
+        count_sold_products = count_sold_items(product['items_sold'])
+        print(f" Модель {product['product']} продано: {count_sold_products}")
+    print()
+
+    all_count_sold_items = 0
+    for product in product_list:
+        count_sold_products = count_sold_items(product['items_sold'])
+        all_count_sold_items += count_sold_products
+        print(
+            f" Модель {product['product']} среднее кол-во продаж: "
+            f"{round(count_sold_products / len(product['items_sold']))}"
+        )
+
+    for product in product_list:
+        count_sold_products = count_sold_items(product['items_sold'])
+        all_count_sold_items += count_sold_products
+    print()
+    print(f"Общее количество проданных товаров: {all_count_sold_items}")
+    print(f"Среднее кол-во продаж всех товаров: {round(all_count_sold_items / len(product_list))}")
+
+
 if __name__ == "__main__":
     main()
